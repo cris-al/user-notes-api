fa81d5243d87711b637f12acf6675bb09bc3ec11
cris-al <albornozcristian94@gmail.com>
2023-01-10 18:31:43 -0300

    cambio en reinicio de base de datos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
     console.log('%s listening at '+port);
   });
