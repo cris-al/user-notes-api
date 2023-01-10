f19d178cf903ccef3ee3a32ba7176d53944b9d63
cris-al <albornozcristian94@gmail.com>
2023-01-10 20:01:03 -0300

    arreglo en coneccion de base de datos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { PORT } = process.env;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT || PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`);
   });
