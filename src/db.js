19659f86c17f55d7606f7426e8a039381475636f
cris-al <albornozcristian94@gmail.com>
2023-01-10 20:15:13 -0300

    arreglo en coneccion a base de datos

--- src/db.js
+++ src/db.js
@@ -3,11 +3,11 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
 let sequelize = new Sequelize(
-                        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+                        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
                         { logging: false, native: false }
                       );
 const basename = path.basename(__filename);
